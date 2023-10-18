c414798c63b9fa4d9ce0353b27ff645748f36069
[James Cameron-Mowat] <69534208+jcm802@users.noreply.github.com>
2023-10-18 19:28:02 +0100

    Syntax error resolved

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Dashboard from './dashboard/dashboard'
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Dashboard />
       </main>
     </>
   )
