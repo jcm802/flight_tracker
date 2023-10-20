1164cb1eefd7ae6dac00c9178e07f1fa882908c2
[James Cameron-Mowat] <69534208+jcm802@users.noreply.github.com>
2023-10-20 22:15:09 +0100

    Added basic profile and settings pags

--- src/pages/dashboard/settings/Settings.tsx
+++ src/pages/dashboard/settings/Settings.tsx
@@ -1,7 +1,102 @@
-import React from 'react'
+import React, { useState, FormEvent, ChangeEvent,  } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Switch,
+  Typography,
+} from "@mui/material";
 
-export default function Settings(): JSX.Element {
+const Settings = () => {
+  const [showRevenue, setShowRevenue] = useState<boolean>(true);
+  const [showProfit, setShowProfit] = useState<boolean>(true);
+  const [showOrders, setShowOrders] = useState<boolean>(true);
+  const [showCustomers, setShowCustomers] = useState<boolean>(true);
+
+  const handleShowRevenueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setShowRevenue(event.target.checked);
+  };
+
+  const handleShowProfitChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setShowProfit(event.target.checked);
+  };
+
+  const handleShowOrdersChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setShowOrders(event.target.checked);
+  };
+
+  const handleShowCustomersChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setShowCustomers(event.target.checked);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // TODO: handle form submission
+  };
   return (
-    <div>Settings</div>
-  )
-}
+    <>
+      <h1>Settings</h1>
+      <Box>
+        <Typography variant="h4" gutterBottom>
+          Dashboard Features
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <FormControl component="fieldset">
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={showRevenue}
+                        onChange={handleShowRevenueChange}
+                      />
+                    }
+                    label="Revenue"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={showProfit}
+                        onChange={handleShowProfitChange}
+                      />
+                    }
+                    label="Profit"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={showOrders}
+                        onChange={handleShowOrdersChange}
+                      />
+                    }
+                    label="Orders"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Switch
+                        checked={showCustomers}
+                        onChange={handleShowCustomersChange}
+                      />
+                    }
+                    label="Customers"
+                  />
+                </FormGroup>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" color="primary">
+                Save Settings
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </>
+  );
+};
+
+export default Settings;
